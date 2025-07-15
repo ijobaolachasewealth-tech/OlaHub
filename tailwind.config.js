29632cd4297dc92e263bec976f6f63fe0a146d46
ijobaolachasewealth-tech <ijobaolachasewealth@gmail.com>
2025-07-15 12:22:23 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,22 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'sky-blue': 'rgb(142, 202, 230)',
+        'blue-green': 'rgb(33, 158, 188)',
+        'prussian-blue': 'rgb(2, 48, 71)',
+        'selective-yellow': 'rgb(255, 183, 3)',
+        'ut-orange': 'rgb(251, 133, 0)',
+        'white-custom': 'rgb(255, 255, 255)',
+        'gray-light': 'rgb(248, 249, 250)',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
 
